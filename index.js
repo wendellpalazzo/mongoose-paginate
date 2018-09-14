f2c55151fb69c34403e2a5d9c2571776c67a507e
wendellpalazzo <wendell.palazzo@gmail.com>
2018-09-14 13:48:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,106 @@
-'use strict';
+const Promise = require('bluebird');
 
 /**
- * @package mongoose-paginate
- * @param {Object} [query={}]
- * @param {Object} [options={}]
- * @param {Object|String} [options.select]
- * @param {Object|String} [options.sort]
- * @param {Array|Object|String} [options.populate]
- * @param {Boolean} [options.lean=false]
- * @param {Boolean} [options.leanWithId=true]
- * @param {Number} [options.offset=0] - Use offset or page to set skip position
- * @param {Number} [options.page=1]
- * @param {Number} [options.limit=10]
- * @param {Function} [callback]
+ * @param {Object}              [query={}]
+ * @param {Object}              [options={}]
+ * @param {Object|String}         [options.select]
+ * @param {Object|String}         [options.sort]
+ * @param {Array|Object|String}   [options.populate]
+ * @param {Boolean}               [options.lean=false]
+ * @param {Boolean}               [options.leanWithId=true]
+ * @param {Number}                [options.offset=0] - Use offset or page to set skip position
+ * @param {Number}                [options.page=1]
+ * @param {Number}                [options.limit=10]
+ * @param {Function}            [callback]
+ *
  * @returns {Promise}
  */
-
-function paginate(query, options, callback) {
+function paginate (query, options, callback) {
   query = query || {};
   options = Object.assign({}, paginate.options, options);
-  let select = options.select;
-  let sort = options.sort;
-  let populate = options.populate;
-  let collation = options.collation;
-  let lean = options.lean || false;
-  let leanWithId = options.leanWithId ? options.leanWithId : true;
-  let limit = options.limit ? options.limit : 10;
-  let page, offset, skip, promises;
-  if (options.offset) {
+
+  const select = options.select;
+  const sort = options.sort;
+  const populate = options.populate;
+  const lean = options.lean || false;
+  const collation = options.collation;
+  const leanWithId = options.hasOwnProperty('leanWithId') ? options.leanWithId : true;
+
+  const limit = options.hasOwnProperty('limit') ? options.limit : 10;
+  let skip; let offset; let
+    page;
+
+  if (options.hasOwnProperty('offset')) {
     offset = options.offset;
     skip = offset;
-  } else if (options.page) {
+  } else if (options.hasOwnProperty('page')) {
     page = options.page;
     skip = (page - 1) * limit;
   } else {
-    page = 1;
     offset = 0;
+    page = 1;
     skip = offset;
   }
+
+  const promises = {
+    docs: Promise.resolve([]),
+    count: this.count(query).exec(),
+  };
+
   if (limit) {
-    let docsQuery = this.find(query)
+    var query = this.find(query)
       .select(select)
       .sort(sort)
       .skip(skip)
       .limit(limit)
-      .lean(lean)
-      .collation(collation);
+      .lean(lean);
+
+    if (collation) query.collation(collation);
+
     if (populate) {
-      [].concat(populate).forEach((item) => {
-        docsQuery.populate(item);
+      [].concat(populate).forEach(item => {
+        query.populate(item);
       });
     }
-    promises = {
-      docs: docsQuery.exec(),
-      count: this.count(query).exec()
-    };
+
+    promises.docs = query.exec();
+
     if (lean && leanWithId) {
-      promises.docs = promises.docs.then((docs) => {
-        docs.forEach((doc) => {
+      promises.docs = promises.docs.then(docs => {
+        docs.forEach(doc => {
           doc.id = String(doc._id);
         });
+
         return docs;
       });
     }
   }
-  promises = Object.keys(promises).map((x) => promises[x]);
-  return Promise.all(promises).then((data) => {
-    let result = {
-      docs: data.docs,
-      total: data.count,
-      limit: limit
-    };
-    if (offset !== undefined) {
-      result.offset = offset;
-    }
-    if (page !== undefined) {
-      result.page = page;
-      result.pages = Math.ceil(data.count / limit) || 1;
-    }
-    if (typeof callback === 'function') {
-      return callback(null, result);
-    }
-    let promise = new Promise();
-    promise.resolve(result);
-    return promise;
-  });
+
+  return Promise.props(promises)
+    .then(data => {
+      const result = {
+        docs: data.docs,
+        total: data.count,
+        limit,
+      };
+
+      if (offset !== undefined) {
+        result.offset = offset;
+      }
+
+      if (page !== undefined) {
+        result.page = page;
+        result.pages = Math.ceil(data.count / limit) || 1;
+      }
+
+      return result;
+    })
+    .asCallback(callback);
 }
 
 /**
  * @param {Schema} schema
  */
-
 module.exports = function(schema) {
   schema.statics.paginate = paginate;
 };
